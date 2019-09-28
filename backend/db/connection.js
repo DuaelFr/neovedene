204ebdbb5d267e52eabea44948b388c29ef697d7
Edouard Cunibil <edouard@happyculture.coop>
2019-09-28 22:46:55 +0200

    Fix deprecation warning.

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,6 +1,6 @@
 const monk = require('monk');
 const connectionString = process.env.MONGODB_URI || 'mongo:27017/neovedene';
-const db = monk(connectionString, {authSource:'admin'});
+const db = monk(connectionString, {authSource:'admin', useUnifiedTopology: true});
 
 db.then(() => {
   console.log('Connected correctly to server')
